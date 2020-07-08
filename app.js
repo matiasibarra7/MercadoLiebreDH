3024642be8e1b24b55adf89d471f0da6510202dd
Matias <matiasibarra7@gmail.com>
2020-07-08 17:51:37 -0300

    añadidos login y register

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
 
+
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended:false }))
 
 app.listen(3000, () => {
   console.log("running!");
 });
 
 app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(`${__dirname}/views/register.html`);
+});
+
+app.post("/register", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(`${__dirname}/views/login.html`);
+});
+
+app.post("/login", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
