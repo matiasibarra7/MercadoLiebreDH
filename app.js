c230330cfa3bdd49918d8a0f400ff1d12d61150f
matiasibarra7 <matiasibarra7@gmail.com>
2020-07-08 20:58:53 -0300

    mas cosas

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 
-
-
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended:false }))
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(3000, () => {
   console.log("running!");
@@ -14,18 +12,14 @@ app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+app.post("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
 app.get("/register", (req, res) => {
   res.sendFile(`${__dirname}/views/register.html`);
 });
 
-app.post("/register", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-
 app.get("/login", (req, res) => {
   res.sendFile(`${__dirname}/views/login.html`);
-});
-
-app.post("/login", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
 });
